af10ad3058c51d466fbcfda3634a1b3c68b44279
marouanelankaoui66@gmail.com <daza@DESKTOP-SQO21O6>
2024-07-07 12:14:40 +0100

    adding_options

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ import time
     to take the user name and return it
 """
 def get_user_name():
-    return input("what is your name")
+    return input("what is your name : ")
 
 
 """
@@ -15,12 +15,27 @@ def get_user_name():
 """
 def welcome_message(name):
     
-    sys.stdout.write(f'hello {name}')
+    sys.stdout.write(f'{text2art("hello " + name)}', )
     time.sleep(1)
 
+"""
+    displaying the options available to the user
+"""
+def options():
+    sys.stdout.write("""
+1- generating a random art
+2- from text to draw
+3- from text to art
+4- wizard mode
+""")
+    return int(input("your option here : "))
+
+"""
+    the main function - entry point
+"""
 def main():
     welcome_message(get_user_name())
-    
+    optionn = options()
 
 if __name__ == "__main__":
     main()
