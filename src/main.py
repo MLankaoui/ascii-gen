2cf429191568f971e00696e3c4e403f698b7f921
marouanelankaoui66@gmail.com <daza@DESKTOP-SQO21O6>
2024-07-07 12:07:22 +0100

    welcome_message

--- src/main.py
+++ src/main.py
@@ -1,5 +1,26 @@
+from art import *
+import sys
+import time
+
+
+"""
+    to take the user name and return it
+"""
+def get_user_name():
+    return input("what is your name")
+
+
+"""
+    to welcome the user to the app
+"""
+def welcome_message(name):
+    
+    sys.stdout.write(f'hello {name}')
+    time.sleep(1)
+
 def main():
-    pass
+    welcome_message(get_user_name())
+    
 
 if __name__ == "__main__":
     main()
